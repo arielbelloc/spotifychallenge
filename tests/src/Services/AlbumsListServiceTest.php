cdbe401d0d7cdd1f2c54b5d7f069d437ba2a93e6
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 11:37:30 -0300

    Create test testGetAnotherTestArtistAlbumsSuccess

--- tests/src/Services/AlbumsListServiceTest.php
+++ tests/src/Services/AlbumsListServiceTest.php
@@ -46,4 +46,43 @@ class AlbumsListServiceTest extends PHPUnit_TestCase {
         
         $this->assertEquals($mockResponse, $albumsListClient('TestArtist'));
     }
+
+    public function testGetAnotherTestArtistAlbumsSuccess()
+    {
+        $mockResponse = [
+            [
+                "name" => "Another Album Name",
+                "released" => "10-10-2010",
+                "tracks" => 10,
+                "cover" => [
+                    "height" => 640,
+                    "width" => 640,
+                    "url" => "https://i.scdn.co/image/6c951f3f334e05ffa"
+                ]
+            ],
+        ];
+
+        $artistEntity = $this->prophesize(ArtistEntity::class);
+        $artistEntity->getId()->willReturn('7BTNOkc344wl9rZ4mLUfGk');
+
+        $findFirstArtistByNameRepository = $this->createPartialMock(FindFirstArtistByNameRepository::class, [
+            '__invoke',
+        ]);
+
+        $findFirstArtistByNameRepository
+            ->method('__invoke')
+            ->willReturn($artistEntity->reveal());
+
+        $findAlbumsByArtistIdRepository = $this->createPartialMock(FindAlbumsByArtistIdRepository::class, [
+            '__invoke',
+        ]);
+
+        $findAlbumsByArtistIdRepository
+            ->method('__invoke')
+            ->willReturn($mockResponse);
+        
+        $albumsListClient = new AlbumsListService($findFirstArtistByNameRepository, $findAlbumsByArtistIdRepository);
+        
+        $this->assertEquals($mockResponse, $albumsListClient('TestArtist'));
+    }
 }
