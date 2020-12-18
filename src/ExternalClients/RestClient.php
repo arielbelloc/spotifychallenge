47d23707e8b51d52aad09cc5522808e9449e9456
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 15:34:47 -0300

    Create external conection

--- src/ExternalClients/RestClient.php
+++ src/ExternalClients/RestClient.php
@@ -4,6 +4,9 @@ namespace App\ExternalClients;
 use GuzzleHttp\Client;
 
 class RestClient {
+    const GET_METHOD = 'GET';
+    const POST_METHOD = 'POST';
+
     private $client;
 
     public function __construct(Client $client)
@@ -11,13 +14,33 @@ class RestClient {
         $this->client = $client;
     }
 
+    public function call(string $method, string $endpoint, array $options) : Array
+    {
+        try{
+            $rawResponse = $this->client->request($method, $endpoint, $options);
+            $body = json_decode(trim($rawResponse->getBody()->getContents()), true);
+        } catch (\GuzzleHttp\Exception\ClientException $e)  {
+            if ($e->getResponse()) {
+                $body = json_decode(trim($e->getResponse()->getBody()->getContents()));
+            } else {
+                throw new \Exception('external api error', 500);
+            }
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+            throw new \Exception('external api error', 500);
+        } catch (\Exception $e) {
+            throw new \Exception('external api error', 500);
+        }
+
+        return $body;
+    }
+
     public function get(string $endpoint, array $options) : Array
     {
-        return [];
+        return $this->call(self::GET_METHOD, $endpoint, $options);
     }
 
     public function post(string $endpoint, array $options) : Array
     {
-        return [];
+        return $this->call(self::POST_METHOD, $endpoint, $options);
     }
 }
