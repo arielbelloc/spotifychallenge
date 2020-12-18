49c4c5f53d4be52110f211f2e57ee8ad8efecad2
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 14:49:47 -0300

    Create test testEndpointSuccess

--- tests/src/Repositories/FindFirstArtistByNameRepositoryTest.php
+++ tests/src/Repositories/FindFirstArtistByNameRepositoryTest.php
@@ -43,4 +43,34 @@ class FindFirstArtistByNameRepositoryTest extends PHPUnit_TestCase {
         $this->assertInstanceOf(ArtistEntity::class, $artist);
         $this->assertEquals('1MuQ2m2tg7naeRGAOxYZer', $artist->getId());
     }
+
+    public function testEndpointSuccess()
+    {
+        $mockResponse = '{
+            "artists": {
+                "items": [
+                    {
+                        "id": "1MuQ2m2tg7naeRGAOxYZer"
+                    }
+                ]
+            }
+        }';
+
+        $restClient = $this->prophesize(RestClient::class);
+        $restClient->get('https://api.spotify.com/v1/search', Argument::any())->willReturn(json_decode($mockResponse, true));
+
+        $getTokenRepository = $this->createPartialMock(GetTokenRepository::class, [
+            '__invoke',
+        ]);
+
+        $getTokenRepository
+            ->method('__invoke')
+            ->willReturn('BQBHvl2tVOM-WarEF3jr2iGOgv7neugpIQD6Ho8vQ4dsRKu_vo8pI3mcbwg1yblgwfO73Z5YMH5HmYPabCE');
+
+        $findArtistByNameRepository = new FindFirstArtistByNameRepository($restClient->reveal(), $getTokenRepository);
+
+        $artist =  $findArtistByNameRepository('Luis Alberto Spinetta');
+        $this->assertInstanceOf(ArtistEntity::class, $artist);
+        $this->assertEquals('1MuQ2m2tg7naeRGAOxYZer', $artist->getId());
+    }
 }
