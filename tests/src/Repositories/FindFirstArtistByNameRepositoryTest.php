878d358c839d55d67b7ecd5c312945528a963a2b
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 15:14:54 -0300

    Fix in test testRequestOptionsSuccess

--- tests/src/Repositories/FindFirstArtistByNameRepositoryTest.php
+++ tests/src/Repositories/FindFirstArtistByNameRepositoryTest.php
@@ -73,4 +73,50 @@ class FindFirstArtistByNameRepositoryTest extends PHPUnit_TestCase {
         $this->assertInstanceOf(ArtistEntity::class, $artist);
         $this->assertEquals('1MuQ2m2tg7naeRGAOxYZer', $artist->getId());
     }
+
+    public function testRequestOptionsSuccess()
+    {
+        $mockResponse = '{
+            "artists": {
+                "items": [
+                    {
+                        "id": "1MuQ2m2tg7naeRGAOxYZer"
+                    }
+                ]
+            }
+        }';
+
+        $restClient = $this->prophesize(RestClient::class);
+
+        $mockToken = 'BQBHvl2tVOM-WarEF3jr2iGOgv7neugpIQD6Ho8vQ4dsRKu_vo8pI3mcbwg1yblgwfO73Z5YMH5HmYPabCE';
+        
+        /** TODO: Do this mocking a config file */
+        $options = [
+            'query' => [
+                'q' => 'Luis Alberto Spinetta',
+                'type' => 'artist',
+                'limit' => '10',
+                'offset' => '0',
+            ],
+            'headers' => [
+                'Authorization' => 'Bearer ' . $mockToken
+            ]
+        ];
+
+        $restClient->get(Argument::any(), $options)->willReturn(json_decode($mockResponse, true));
+
+        $getTokenRepository = $this->createPartialMock(GetTokenRepository::class, [
+            '__invoke',
+        ]);
+
+        $getTokenRepository
+            ->method('__invoke')
+            ->willReturn($mockToken);
+
+        $findArtistByNameRepository = new FindFirstArtistByNameRepository($restClient->reveal(), $getTokenRepository);
+
+        $artist =  $findArtistByNameRepository('Luis Alberto Spinetta');
+        $this->assertInstanceOf(ArtistEntity::class, $artist);
+        $this->assertEquals('1MuQ2m2tg7naeRGAOxYZer', $artist->getId());
+    }
 }
