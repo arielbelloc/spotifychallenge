3b50197ddc2635a68554643a3939aa27ff423344
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 15:03:43 -0300

    create test testRequestOptionsSuccess

--- tests/src/Repositories/FindAlbumsByArtistIdRepositoryTest.php
+++ tests/src/Repositories/FindAlbumsByArtistIdRepositoryTest.php
@@ -233,4 +233,69 @@ class FindAlbumsByArtistIdRepositoryTest extends PHPUnit_TestCase {
 
         $this->assertEquals($mockResponse, $findAlbumsByArtistIdRepository('6qqNVTkY8uBg9cP3Jd7DAH'));       
     }
+
+    public function testRequestOptionsSuccess()
+    {
+        $serviceResponse = '{
+            "items": [
+                {
+                    "images": [
+                        {
+                            "height": 640,
+                            "url": "https://i.scdn.co/image/ab67616d0000b27350a3147b4edd7701a876c6ce",
+                            "width": 640
+                        }
+                    ],
+                    "name": "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?",
+                    "release_date": "2019-03-29",
+                    "total_tracks": 14
+                }
+            ],
+            "limit": 50,
+            "next": null
+        }';
+
+        $restClient = $this->prophesize(RestClient::class);
+
+        $mockToken = 'BQBHvl2tVOM-WarEF3jr2iGOgv7neugpIQD6Ho8vQ4dsRKu_vo8pI3mcbwg1yblgwfO73Z5YMH5HmYPabCE';
+
+        /** TODO: Do this mocking a config file */
+        $options = [
+            'query' => [
+                'album_type' => 'album',
+                'offset' => 0,
+                'limmit' => 20
+            ],
+            'headers' => [
+                'Authorization' => 'Bearer ' . $mockToken
+            ]
+        ];
+
+        $restClient->get(Argument::any(), $options)->willReturn(json_decode($serviceResponse, true));
+
+        $getTokenRepository = $this->createPartialMock(GetTokenRepository::class, [
+            '__invoke',
+        ]);
+
+        $getTokenRepository
+            ->method('__invoke')
+            ->willReturn($mockToken);
+
+        $findAlbumsByArtistIdRepository = new FindAlbumsByArtistIdRepository($restClient->reveal(), $getTokenRepository);
+
+        $mockResponse = [
+            [
+                'name' => 'WHEN WE ALL FALL ASLEEP, WHERE DO WE GO?',
+                'released' => '2019-03-29',
+                'tracks' => 14,
+                'cover' => [
+                    'height' => 640,
+                    'url' => 'https://i.scdn.co/image/ab67616d0000b27350a3147b4edd7701a876c6ce',
+                    'width' => 640
+                ],
+            ]
+        ];
+
+        $this->assertEquals($mockResponse, $findAlbumsByArtistIdRepository('6qqNVTkY8uBg9cP3Jd7DAH'));       
+    }
 }
