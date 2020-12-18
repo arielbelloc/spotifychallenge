29a14cc05593cb1aa79d960a67f447c5ea1e3195
Ariel Belloc <bellocari@globalhitss.com>
2020-12-18 11:39:51 -0300

    Remove AlbumsListService mocks and use inject services

--- src/Services/AlbumsListService.php
+++ src/Services/AlbumsListService.php
@@ -17,17 +17,9 @@ class AlbumsListService {
 
     public function __invoke($artistName) : array
     {
-        return  [
-            [
-                "name" => "Album Name",
-                "released" => "10-10-2010",
-                "tracks" => 10,
-                "cover" => [
-                    "height" => 640,
-                    "width" => 640,
-                    "url" => "https://i.scdn.co/image/6c951f3f334e05ffa"
-                ]
-            ],
-        ];
+        $artist = ($this->findFirstArtistByNameRepository)($artistName);
+        $ambumList = ($this->findAlbumsByArtistIdRepository)($artist->getId());
+
+        return $ambumList;
     }
 }
